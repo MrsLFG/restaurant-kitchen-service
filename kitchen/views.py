6ee5ca7ab8be3a84d9186074cfe6bc2819ec0593
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-04 12:29:57 +0300

    add in restaurant_kitchen_service/urls.py path include kitchen.urls & write index function in views.py

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_cook = Cook.objects.count()
+    num_dish = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    context = {
+        "num_cook": num_cook,
+        "num_dish": num_dish,
+        "Num_dish_types": num_dish_types,
+    }
+    return render(request, "kitchen/index.html", context=context)
