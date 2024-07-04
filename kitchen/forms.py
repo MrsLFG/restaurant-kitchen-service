d6a56bf27dbcb47fef150944a2619ed671c782d7
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-04 17:26:03 +0300

    added form Create/Update for CookViews

---
+++ kitchen/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from kitchen.models import Cook
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = (UserCreationForm.Meta.fields +
+                  ("first_name", "last_name", "years_of_experience",))
+
+
+class CookUpdateExperience(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ("years_of_experience", )
