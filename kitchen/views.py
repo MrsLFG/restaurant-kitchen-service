d3459b08bc5b9b236a4b9bb6740611e1a7e546da
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-07 10:00:09 +0300

    update pagination for DishListView

--- kitchen/views.py
+++ kitchen/views.py
@@ -118,7 +118,7 @@ class CookDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
-    paginate_by = 3
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DishListView, self).get_context_data(**kwargs)
