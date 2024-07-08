13f7efd32b251402e7af09e2bcd997f180af4561
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-08 09:58:34 +0300

    page for dish-types done with free templates & pagination changed from 3 to 4

--- kitchen/views.py
+++ kitchen/views.py
@@ -36,7 +36,7 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     template_name = "kitchen/dish_type_list.html"
     context_object_name = "dish_type_list"
-    paginate_by = 3
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DishTypeListView, self).get_context_data(**kwargs)
