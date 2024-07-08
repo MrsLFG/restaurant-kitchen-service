020fed8f26ae577ddd7c8b12d2ef4007f925f171
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-08 10:17:29 +0300

    replace pagination from 3 to 4 in CookListView

--- kitchen/views.py
+++ kitchen/views.py
@@ -76,7 +76,7 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
-    paginate_by = 3
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(CookListView, self).get_context_data(**kwargs)
